3191f9dec7d01988a7161182b074e91f9b33747e
Junyi <yunyi.xie@outlook.com>
2021-04-04 14:30:39 +0200

    whitespace

--- dashboard.php
+++ dashboard.php
@@ -1,20 +1,20 @@
 <?php 
 /* Copyright (c) - 2021 by Junyi Xie */	
 
-include_once("inc/connect.php");
-include_once("inc/functions.php");
-include_once("inc/class.php");
+    include_once("inc/connect.php");
+    include_once("inc/functions.php");
+    include_once("inc/class.php");
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Sopranos</title>
-<meta charset="UTF-8">
-<meta name="author" content="Junyi Xie">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="shortcut icon" href="assets/images/favicon/favicon.ico" type="image/x-icon">
-<link rel="stylesheet" type="text/css" href="assets/css/style.css?<?php echo date("YmdHis"); ?>" media="screen">
+    <title>Sopranos</title>
+    <meta charset="UTF-8">
+    <meta name="author" content="Junyi Xie">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/images/favicon/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css?<?php echo date("YmdHis"); ?>" media="screen">
 </head>
 <body class="js-body">
 
